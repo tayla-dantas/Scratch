8f55b0d19809eddf3ba2e5954bbcb8479f6e7354
Tayla Tineu <dantas.tayla@gmail.com>
2018-10-17 01:37:39 -0300

    Initial commit

---
+++ Tales of seasons/Arte_finalizada/tilesets/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="62a865873467661de31790f374f11c41" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="62a865873467661de31790f374f11c41.png" width="512" height="512"/>
+</tileset>
